863a4a58634a065e42542cd2adf53af5483d0270
Christophe Beyer <cbeyer@free.fr>
2023-02-06 13:06:03 +0100

    Update DistanceMatrixService.php

--- src/Service/DistanceMatrix/DistanceMatrixService.php
+++ src/Service/DistanceMatrix/DistanceMatrixService.php
@@ -14,8 +14,13 @@ namespace Ivory\GoogleMap\Service\DistanceMatrix;
 use Http\Client\HttpClient;
 use Http\Message\MessageFactory;
 use Ivory\GoogleMap\Service\AbstractSerializableService;
+use Ivory\GoogleMap\Service\Base\Distance;
+use Ivory\GoogleMap\Service\Base\Duration;
 use Ivory\GoogleMap\Service\DistanceMatrix\Request\DistanceMatrixRequestInterface;
+use Ivory\GoogleMap\Service\DistanceMatrix\Response\DistanceMatrixElement;
+use Ivory\GoogleMap\Service\DistanceMatrix\Response\DistanceMatrixElementStatus;
 use Ivory\GoogleMap\Service\DistanceMatrix\Response\DistanceMatrixResponse;
+use Ivory\GoogleMap\Service\DistanceMatrix\Response\DistanceMatrixRow;
 use Ivory\Serializer\Context\Context;
 use Ivory\Serializer\Naming\SnakeCaseNamingStrategy;
 use Ivory\Serializer\SerializerInterface;
@@ -45,12 +50,26 @@ class DistanceMatrixService extends AbstractSerializableService
     {
         $httpRequest = $this->createRequest($request);
         $httpResponse = $this->getClient()->sendRequest($httpRequest);
+        
+        $array = json_decode($httpResponse->getBody(), true);
 
-        $response = $this->deserialize(
-            $httpResponse,
-            DistanceMatrixResponse::class,
-            (new Context())->setNamingStrategy(new SnakeCaseNamingStrategy())
-        );
+        $response = new DistanceMatrixResponse();
+        $response->setOrigins($array['origin_addresses']);
+        $response->setDestinations($array['destination_addresses']);
+
+        foreach ($array['rows'] as $row) {
+            $distanceMatrixRow  = new DistanceMatrixRow();
+            foreach ($row['elements'] as $element) {
+                $distanceMatrixElement  = new DistanceMatrixElement();
+                $distanceMatrixElement->setStatus($element['status']);
+                $distance = new Distance($element['distance']['value'], $element['distance']['text']);
+                $distanceMatrixElement->setDistance($distance);
+                $duration = new Duration($element['duration']['value'], $element['duration']['text']);
+                $distanceMatrixElement->setDuration($duration);
+                $distanceMatrixRow->addElement($distanceMatrixElement);
+            }
+            $response->addRow($distanceMatrixRow);
+        }
 
         $response->setRequest($request);
 
