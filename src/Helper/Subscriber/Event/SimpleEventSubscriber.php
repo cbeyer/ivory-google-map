7326ac6eea3b066e21ffba232ae80ca343f7777f
Christophe Beyer <cbeyer@free.fr>
2023-02-06 11:52:56 +0100

    Update SimpleEventSubscriber.php

--- src/Helper/Subscriber/Event/SimpleEventSubscriber.php
+++ src/Helper/Subscriber/Event/SimpleEventSubscriber.php
@@ -88,7 +88,7 @@ class SimpleEventSubscriber extends AbstractSubscriber
     /**
      * {@inheritdoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [MapEvents::JAVASCRIPT_EVENT_EVENT => 'handleMap'];
     }
