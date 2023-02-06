f94227a676ad10f771086d0c396a0a44c04ef3b5
Christophe Beyer <cbeyer@free.fr>
2023-02-06 12:14:16 +0100

    Update SymbolSubscriber.php

--- src/Helper/Subscriber/Overlay/SymbolSubscriber.php
+++ src/Helper/Subscriber/Overlay/SymbolSubscriber.php
@@ -88,7 +88,7 @@ class SymbolSubscriber extends AbstractSubscriber
     /**
      * {@inheritdoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [MapEvents::JAVASCRIPT_OVERLAY_SYMBOL => 'handleMap'];
     }
