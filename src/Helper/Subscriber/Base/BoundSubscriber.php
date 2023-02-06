bcdfd25706164fd6409032158c705819f20517f2
Christophe Beyer <cbeyer@free.fr>
2023-02-06 11:50:30 +0100

    Update BoundSubscriber.php

--- src/Helper/Subscriber/Base/BoundSubscriber.php
+++ src/Helper/Subscriber/Base/BoundSubscriber.php
@@ -88,7 +88,7 @@ class BoundSubscriber extends AbstractSubscriber
     /**
      * {@inheritdoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [MapEvents::JAVASCRIPT_BASE_BOUND => 'handleMap'];
     }
