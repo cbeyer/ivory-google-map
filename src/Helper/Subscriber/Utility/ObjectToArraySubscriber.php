ba38b1b559bda02f42ecb284609a967f4dfbf052
Christophe Beyer <cbeyer@free.fr>
2023-02-06 12:17:01 +0100

    Update ObjectToArraySubscriber.php

--- src/Helper/Subscriber/Utility/ObjectToArraySubscriber.php
+++ src/Helper/Subscriber/Utility/ObjectToArraySubscriber.php
@@ -59,7 +59,7 @@ class ObjectToArraySubscriber extends AbstractSubscriber
     /**
      * {@inheritdoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [MapEvents::JAVASCRIPT_INIT_FUNCTION => 'handleMap'];
     }
