33518bc5c4e459f202597ced4e7664d5d0dc95f9
Christophe Beyer <cbeyer@free.fr>
2023-02-06 11:53:41 +0100

    Update KmlLayerSubscriber.php

--- src/Helper/Subscriber/Layer/KmlLayerSubscriber.php
+++ src/Helper/Subscriber/Layer/KmlLayerSubscriber.php
@@ -88,7 +88,7 @@ class KmlLayerSubscriber extends AbstractSubscriber
     /**
      * {@inheritdoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [MapEvents::JAVASCRIPT_LAYER_KML_LAYER => 'handleMap'];
     }
