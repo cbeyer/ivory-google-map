caec6cc8a95eb9eca83806f5430557202e4c1a13
Christophe Beyer <cbeyer@free.fr>
2023-02-06 12:13:09 +0100

    Update MarkerShapeSubscriber.php

--- src/Helper/Subscriber/Overlay/MarkerShapeSubscriber.php
+++ src/Helper/Subscriber/Overlay/MarkerShapeSubscriber.php
@@ -88,7 +88,7 @@ class MarkerShapeSubscriber extends AbstractSubscriber
     /**
      * {@inheritdoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [MapEvents::JAVASCRIPT_OVERLAY_MARKER_SHAPE => 'handleMap'];
     }
