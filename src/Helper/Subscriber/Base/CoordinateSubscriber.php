8d0a2bb6b08ad95030e46758819b719cc2c115a2
Christophe Beyer <cbeyer@free.fr>
2023-02-06 11:50:43 +0100

    Update CoordinateSubscriber.php

--- src/Helper/Subscriber/Base/CoordinateSubscriber.php
+++ src/Helper/Subscriber/Base/CoordinateSubscriber.php
@@ -88,7 +88,7 @@ class CoordinateSubscriber extends AbstractSubscriber
     /**
      * {@inheritdoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [MapEvents::JAVASCRIPT_BASE_COORDINATE => 'handleMap'];
     }
