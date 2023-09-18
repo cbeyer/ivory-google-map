0d78b7d38eee15724871abed1173e7e28f5e48cc
Christophe Beyer <cbeyer@free.fr>
2023-09-18 07:04:31 +0200

    Update FormatSubscriber.php

--- src/Helper/Subscriber/Image/FormatSubscriber.php
+++ src/Helper/Subscriber/Image/FormatSubscriber.php
@@ -32,7 +32,7 @@ class FormatSubscriber implements EventSubscriberInterface
     /**
      * {@inheritdoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [StaticMapEvents::FORMAT => 'handleMap'];
     }
