fc1f43e4393d233ab20050e096e84a634bd4f0b0
Christophe Beyer <cbeyer@free.fr>
2023-09-18 07:06:40 +0200

    Update TypeSubscriber.php

--- src/Helper/Subscriber/Image/TypeSubscriber.php
+++ src/Helper/Subscriber/Image/TypeSubscriber.php
@@ -38,7 +38,7 @@ class TypeSubscriber implements EventSubscriberInterface
     /**
      * {@inheritdoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [StaticMapEvents::TYPE => 'handleMap'];
     }
