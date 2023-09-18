a785701b342aced1344d6b509ee445137504a574
Christophe Beyer <cbeyer@free.fr>
2023-09-18 07:04:45 +0200

    Update KeySubscriber.php

--- src/Helper/Subscriber/Image/KeySubscriber.php
+++ src/Helper/Subscriber/Image/KeySubscriber.php
@@ -43,7 +43,7 @@ class KeySubscriber implements EventSubscriberInterface
     /**
      * {@inheritdoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [StaticMapEvents::KEY => 'handleMap'];
     }
